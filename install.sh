62dbd416a7e828376bbe10fcc6b668c92da75aa8
Rajasimman S <srajasimman@gmail.com>
2023-08-12 11:23:39 +0530

    :bug: fix

--- install.sh
+++ install.sh
@@ -16,10 +16,21 @@ if [ "$EUID" -ne 0 ]
 fi
 cd /tmp/ || exit
 
+sudo apt update
+sudo apt-get install -y \
+    apt-transport-https \
+    software-properties-common \
+    gnupg \
+    ca-certificates \
+    lsb-release \
+    curl \
+    wget \
+    unzip \
+    tar \
+    sudo
+
 function install_docker () {
     echo "Installing docker and docker-compose"
-    sudo apt update
-    sudo apt-get install apt-transport-https ca-certificates curl software-properties-common curl gnupg ca-certificates lsb-release -y
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt update
